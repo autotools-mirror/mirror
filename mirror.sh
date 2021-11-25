ce45cc83a6d243290282d640a4be10cbc5b8e542
Timothy Gu <timothygu99@gmail.com>
2021-11-24 17:55:21 -0800

    Use HTTPS for Git clones

--- mirror.sh
+++ mirror.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Repo list
 
+cd /mirror/mirror
+
 # import
 . git_login.sh
 . git_mirror.sh
 
 # repos
-git_mirror autoconf git git://git.savannah.gnu.org/autoconf.git
-git_mirror automake git git://git.savannah.gnu.org/automake.git
-git_mirror gettext git git://git.savannah.gnu.org/gettext.git
-git_mirror libtool git git://git.savannah.gnu.org/libtool.git
-git_mirror m4 git git://git.savannah.gnu.org/m4.git
+git_mirror autoconf git https://git.savannah.gnu.org/git/autoconf.git
+git_mirror automake git https://git.savannah.gnu.org/git/automake.git
+git_mirror gettext git https://git.savannah.gnu.org/git/gettext.git
+git_mirror libtool git https://git.savannah.gnu.org/git/libtool.git
+git_mirror m4 git https://git.savannah.gnu.org/git/m4.git
